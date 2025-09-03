fe92969ad9cfacdb6056ca43026201f489a62227
Raamsri Kumar <raam@tinkershack.in>
2025-09-03 15:04:14 +0530

    Define constants for System API
    Signed-off-by: Raamsri Kumar <raam@tinkershack.in>

---
+++ proto/system_command_types.go
@@ -0,0 +1,46 @@
+// Copyright 2025 Raamsri Kumar <raam@tinkershack.in>
+// Copyright 2025 The StrataSTOR Authors and Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package proto
+
+// System management command constants for Toggle gRPC communication
+const (
+	// System information commands
+	CmdSystemInfoGet         = "system.info.get"
+	CmdSystemInfoCPUGet      = "system.info.cpu.get"
+	CmdSystemInfoMemoryGet   = "system.info.memory.get"
+	CmdSystemInfoOSGet       = "system.info.os.get"
+	CmdSystemInfoPerformanceGet = "system.info.performance.get"
+	CmdSystemHealthGet       = "system.health.get"
+
+	// Hostname management commands
+	CmdSystemHostnameGet = "system.hostname.get"
+	CmdSystemHostnameSet = "system.hostname.set"
+
+	// User management commands
+	CmdSystemUsersList   = "system.users.list"
+	CmdSystemUsersCreate = "system.users.create"
+	CmdSystemUsersDelete = "system.users.delete"
+	CmdSystemUsersGet    = "system.users.get"
+
+	// Group management commands
+	CmdSystemGroupsList   = "system.groups.list"
+	CmdSystemGroupsGet    = "system.groups.get"
+	CmdSystemGroupsCreate = "system.groups.create"
+	CmdSystemGroupsDelete = "system.groups.delete"
+
+	// Power management commands
+	CmdSystemPowerStatusGet        = "system.power.status.get"
+	CmdSystemPowerScheduledGet     = "system.power.scheduled.get"
+	CmdSystemPowerShutdown         = "system.power.shutdown"
+	CmdSystemPowerReboot           = "system.power.reboot"
+	CmdSystemPowerScheduleShutdown = "system.power.schedule-shutdown.create"
+	CmdSystemPowerScheduledDelete  = "system.power.scheduled.delete"
+
+	// System configuration commands
+	CmdSystemTimezoneGet = "system.config.timezone.get"
+	CmdSystemTimezoneSet = "system.config.timezone.set"
+	CmdSystemLocaleGet   = "system.config.locale.get"
+	CmdSystemLocaleSet   = "system.config.locale.set"
+)
