d59227537a7a7f2c9b5114c25c054616f27bc0a6
Raamsri Kumar <raam@tinkershack.in>
2025-11-20 00:03:14 +0530

    Add inventory commands
    Signed-off-by: Raamsri Kumar <raam@tinkershack.in>

---
+++ proto/inventory_command_types.go
@@ -0,0 +1,11 @@
+// Copyright 2025 Raamsri Kumar <raam@tinkershack.in>
+// Copyright 2025 The StrataSTOR Authors and Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package proto
+
+// Inventory command constants for Toggle gRPC communication
+const (
+	// CmdInventoryGet retrieves the complete Rodent inventory
+	CmdInventoryGet = "inventory.get"
+)
