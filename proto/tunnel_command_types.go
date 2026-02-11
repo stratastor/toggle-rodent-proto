6f1005cc615488363b324de50cd06bab6676e835
Raamsri Kumar <raamsrik@gmail.com>
2026-02-11 17:05:18 +0530

    Add tunnel HTTP proxy command type
    Add CmdTunnelHTTP constant for proxying HTTP
    requests to local services on Rodent through
    the existing gRPC bidirectional stream.

---
+++ proto/tunnel_command_types.go
@@ -0,0 +1,11 @@
+// Copyright 2025 Raamsri Kumar <raam@tinkershack.in>
+// Copyright 2025 The StrataSTOR Authors and Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package proto
+
+// Tunnel command constants for Toggle gRPC communication
+const (
+	// CmdTunnelHTTP proxies an HTTP request to a local service on Rodent
+	CmdTunnelHTTP = "tunnel.http"
+)
